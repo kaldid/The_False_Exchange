f4dd67005244ba598dc5752c14319c9a49dd6978
devashish <devashish>
2025-04-27 15:06:31 +0530

    backend,auth

---
+++ backend/controllers/UserControllers.js
@@ -0,0 +1,86 @@
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import User from '../models/User.js'
+
+
+
+const registerUser = async (req,res)=>{
+    try {
+        const {username,email,password} = req.body;
+        if(!username || !email || ! password){
+            return res.json({success:false,message:"Missing Details"})
+
+        }
+        const existingUser = User.find({username:username});
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        // object that store user information 
+
+        const userData ={
+            username,email,password:hashedPassword
+        } 
+        const newUser = new User(userData) // mew user store details in mondobv data baser 
+
+        const user = await newUser.save()
+
+        res.json({ message: 'Signup successful' });
+
+        // const token  = jwt.sign({id:user._id},process.env.JWT_SECRET)
+
+        // res.json({success:true,token,user:{name:user.username}})
+
+
+
+        
+    } catch (error) {
+
+        console.log(error)
+        res.json({success:false,message:error.message})
+        
+    }
+}
+
+const loginUser  = async(req,res)=>{
+    try {
+        const{username,password} = req.body;
+
+        const user = await User.findOne({username});
+        if(!user){
+            res.json({success:false,message:"User does not exist"})
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password)
+        
+        if(isMatch){
+            const payload={username:username,
+                email:user.email}
+            const token  = jwt.sign(payload,process.env.JWT_SECRET)
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false, // true if HTTPS (for local dev, keep false)
+                sameSite: 'Strict',
+                maxAge: 60 * 60 * 1000, // 1 hour
+              });
+        }
+        else{
+            res.json({success:false,message:"Invalid Credientials"})
+
+
+        } 
+        res.json({ message: 'Login successful' });
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+        
+    }
+}
+
+export{loginUser,registerUser}
